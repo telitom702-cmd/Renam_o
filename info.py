5f0d2b0e456af7339907a7f8b433b0da98db8120
telitom702-cmd <telitom702@gmail.com>
2026-09-15 21:57:09 +0600

    Add configuration variables for API and database

---
+++ info.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = 12345678  # আপনার API_ID
+API_HASH = "your_api_hash_here"
+BOT_TOKEN = "your_bot_token_here"
+MONGO_URI = "mongodb+srv://user:password@cluster.mongodb.net/?retryWrites=true&w=majority"
+ADMIN_ID = 123456789
